944d5495d424ac925c01e651b9a39632e687f1ee
Jules ROY <julesroydev@gmail.com>
2022-06-24 16:29:49 +0200

    Amélioration de l'affichage, RC

--- projetsapp/projetapppalindrome/apppalindrome.py
+++ projetsapp/projetapppalindrome/apppalindrome.py
@@ -1,26 +1,25 @@
 from tkinter import *
-from turtle import bgcolor
 
 app = Tk()
 app.title("Vérification palindrome")
-app.geometry("320x120")
+app.geometry("300x140")
 app.resizable(width=False, height=False)
 app.configure(bg='#21272c')
 txt = Entry(app) #entrée utilisateur
 ch = Label(app) #label qui affiche le resultat, vide au demarrage
 
-txt.grid(row=0, column=0)
+txt.pack(side=TOP, padx=5, pady=5)
 txt.focus_force() #force le curseur (oblige l'activation de la barre)
-ch.grid(row=1, column=0)
-ch.configure(bg='#21272c', fg="white")
 
 #fonction de verification du palindrome
 def palindrome():
-    ch.grid(row=3, column=0) #permet de réafficher un resultat si la fonction de nettoyage d'écran a été utilisée
     mot = txt.get()
     mot = mot.lower()
     if mot == "":
         reponse = "Veuillez indiquez un mot ou un nombre"
+        ch.configure(text = reponse)
+    elif len(mot) > 22:
+        reponse = "Ce mot est trop long"
         ch.configure(text = reponse)
     else:
         motinverse = ''.join(reversed(mot))
@@ -31,12 +30,17 @@ def palindrome():
             reponse = mot + " n'est pas un palindrome"
             ch.configure(text = reponse)
 
-verifier = Button(app,text='Vérifier', anchor='center', command=palindrome).grid(row=1 , column=0) # bouton qui lance la commande
+verifier = Button(app,text='Vérifier', anchor='center', command=palindrome).pack() #bouton qui lance la commande
 
 #fonction de "nettoyage" de l'ecran
 def clear():
-    ch.grid_forget()
+    ch.pack_forget()
     txt.delete("0","end")
+    ch.pack() #permet de réafficher un resultat si la fonction de nettoyage d'écran a été utilisée
 
-vider = Button(app,text='Vider', anchor='center', command=clear).grid(row=2 , column=0) # bouton qui lance la commande
+vider = Button(app,text='Vider', anchor='center', command=clear).pack() #bouton qui lance la commande
+
+ch.pack()
+ch.configure(bg='#21272c', fg="white")
+
 app.mainloop()
